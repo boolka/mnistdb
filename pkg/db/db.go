97b2d8e1cd1d80b97a042463a35211711d3eaa0e
Ragim Ibragimov <boolka989@gmail.com>
2025-01-20 15:07:45 +0300

    feat: add download url

---
+++ pkg/db/db.go
@@ -0,0 +1,10 @@
+package db
+
+const MnistDbUrl = "https://raw.githubusercontent.com/boolka/mnistdb/refs/heads/main/cmd/mnistdb/db/"
+
+const (
+	TrainImagesDb = "train-images.idx3-ubyte"
+	TrainLabelsDb = "train-labels.idx1-ubyte"
+	TestImagesDb  = "t10k-images.idx3-ubyte"
+	TestLabelsDb  = "t10k-labels.idx1-ubyte"
+)
